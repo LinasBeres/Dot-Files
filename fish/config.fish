bd9478dba2e4167a5ec6ff4b59cd385f71d8e52e
Linas Beresna <beres.linas@gmail.com>
2022-06-06 19:02:33 -0700

    Updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -29,11 +29,19 @@ set fish_pager_color_secondary --background=normal
 function fish_prompt
   set last_status $status
 
-	echo -n (set_color --bold grey)'|'(set_color --bold yellow)$HOST(set_color --bold grey)'|'
+  # rez env amount
+  echo -n (set_color -o red)$REZ_ENV_PROMPT' '
 
+  # username@hostname
+  if [ (hostname -s) != "cs-gruvi-53s" ]
+	  echo -n (set_color -o grey)'|'(set_color -o yellow)(whoami)(set_color -o red)@(set_color -o yellow)(hostname -s)(set_color -o grey)'|'
+  end
+
+  # pwd
 	echo -n ' '(set_color cyan)(prompt_pwd) ''
   set_color -o normal
 
+  # my classic fish ">>>" prompt
 	echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color blue)'❯ '
 
   set_color normal
@@ -56,20 +64,23 @@ function fish_greeting
   title
 end
 
-# set -xU LESS_TERMCAP_md (printf "\e[01;31m")
-# set -xU LESS_TERMCAP_me (printf "\e[0m")
-# set -xU LESS_TERMCAP_se (printf "\e[0m")
-# set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")
-# set -xU LESS_TERMCAP_ue (printf "\e[0m")
-# set -xU LESS_TERMCAP_us (printf "\e[01;32m")
-
-set SASS_LIBSASS_PATH '/usr/local/lib/libsass'
-
 set fish_color_param 427b58
 
+
+###################################################
+########## PATHS & ENVIRON VARIABLES ##############
+###################################################
+
+# Path where my custom binaries are at
 set -gx PATH $PATH $HOME/bin
 
-set FZF_DEFAULT_COMMAND ag
+# Defualt command for fzf to use
+set FZF_DEFAULT_COMMAND rg --files --hidden --smart-case --glob "!.git/*"
+# set FZF_DEFAULT_COMMAND ag
+
+# Tell fish to show useful info on git prompt
+set -g __fish_git_prompt_show_informative_status
+set -g __fish_git_prompt_showcolorhints
 
 ###################################################
 ################### ALIASES #######################
@@ -78,26 +89,50 @@ set FZF_DEFAULT_COMMAND ag
 # Random Aliases for extra stuff
 alias xup="xrdb -load ~/.Xresources"
 alias ls="ls --color=auto"
+alias ll="ls -l --color=auto"
+alias la="ls -la --color=auto"
 alias tree="tree -C"
 
 # New commands if using an offline computer - uncomment if can install the latest versions
 alias cat="$HOME/bin/ccat"
 alias ranger="$HOME/bin/ranger/ranger.py"
 alias ag="$HOME/bin/ag"
-alias ls="ls_extended"
-alias ll="ls_extended -l"
-alias la="ls_extended -al"
-alias fzy="$HOME/bin/fzy"
-alias fish="$HOME/bin/fish-3.0.2/fish"
 
-# alias fn="nvim (fzf)"
+# Alias for ls/ll/la cus normal ls is ancient and sucks
+# alias ls="exa"
+# alias ll="exa -lh"
+# alias la="exa -alh"
+
+# God amognst men
 alias f="fzf"
-# alias nv="$HOME/bin/nvim"
-# alias nvim="$HOME/bin/nvim"
 
 # Git shortcuts
 alias gits="git status -s"
 alias gitd="git diff"
 alias gitall="git add -u; git commit -m"
 
-# command /u/libe/bin/fish-3.0.2/fish
+# Keyboard shortcuts
+alias us="setxkbmap us"
+alias uk="setxkbmap uk"
+
+# Update shortcuts
+alias update="sudo apt update && sudo apt upgrade"
+
+# Functions for Brightness on all monitors
+function bup
+  set --local currentBrightness (xrandr --verbose | awk '/Brightness/ { print $2; exit }')
+  set --local newBrightness (math $currentBrightness + 0.1)
+  set --local monitors (xrandr | grep " connected " | awk '{ print$1 }')
+  for monitor in $monitors;
+    xrandr --output $monitor --brightness $newBrightness
+  end
+end
+
+function bdown
+  set --local currentBrightness (xrandr --verbose | awk '/Brightness/ { print $2; exit }')
+  set --local newBrightness (math $currentBrightness - 0.1)
+  set --local monitors (xrandr | grep " connected " | awk '{ print$1 }')
+  for monitor in $monitors;
+    xrandr --output $monitor --brightness $newBrightness
+  end
+end

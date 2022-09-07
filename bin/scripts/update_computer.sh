a831703a0c74e09ee44dcde3f997bda3f7532a30
Linas Beresna <lberesna@cs-gruvi-53s.cmpt.sfu.ca>
2022-09-07 14:52:43 -0700

    Cleanup bin

--- bin/scripts/update_computer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cp -f tmux.conf ~/.tmux.conf
-cp -f .Xresources ~/.Xresources
-cp -f vim/vimrc ~/.vimrc
-cp -f i3/config ~/.config/i3/config
-cp -f nvim/init.vim ~/.config/nvim/init.vim
-cp -f fish/config.fish ~/.config/fish/config.fish

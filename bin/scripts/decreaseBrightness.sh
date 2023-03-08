f0f6991152ac9905ea96c47c66d574c30353af80
LinasBeres <beres.linas@gmail.com>
2023-03-07 22:39:48 -0800

    Added increase and decrease brightness

---
+++ bin/scripts/decreaseBrightness.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+let "max=$(brightnessctl max)"
+let "current=$(brightnessctl get) * 100"
+
+let "percentage=$current / $max"
+
+if [ "$percentage" -lt 11 ]; then
+	brightnessctl set 1%-
+else
+	brightnessctl set 5%-
+fi
+

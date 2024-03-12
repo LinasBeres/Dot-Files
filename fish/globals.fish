3bcb5add4e663d48e1dba0b117160f1115e73747
LinasBeres <beres.linas@gmail.com>
2024-03-12 16:13:54 -0700

    Use universal variables

--- fish/globals.fish
+++ fish/globals.fish
@@ -4,40 +4,40 @@
 
 # PATH
 if not string match -q -- "$HOME/bin" $PATH
-    set -gx PATH $PATH $HOME/bin
+    set -Ux PATH $PATH $HOME/bin
 end
 
 if not string match -q -- "$HOME/.local/bin" $PATH
-    set -qx PATH $PATH $HOME/.local/bin
+    set -Ux PATH $PATH $HOME/.local/bin
 end
 
 if not string match -q -- "$HOME/.cargo/bin" $PATH
-    set -qx PATH $PATH $HOME/.cargo/bin
+    set -Ux PATH $PATH $HOME/.cargo/bin
 end
 
 if not string match -q -- "$HOME/.fzf/bin" $PATH
-    set -qx PATH $PATH "$HOME/.fzf/bin"
+    set -Ux PATH $PATH "$HOME/.fzf/bin"
 end
 
 # Editor
-set -qx VISUAL (which nvim)
-set -qx EDITOR $VISUAL
+set -Ux VISUAL (which nvim)
+set -Ux EDITOR $VISUAL
 
 # Icon spacing for exa (ls)
-set -qx EXA_ICON_SPACING 2
+set -Ux EXA_ICON_SPACING 2
 
 # Force cmake to create compile_commands.json
-set -qx CMAKE_EXPORT_COMPILE_COMMANDS 1
+set -Ux CMAKE_EXPORT_COMPILE_COMMANDS 1
 
 # fzf use rg/ag
 if type -q rg
-    set -qx FZF_DEFAULT_COMMAND rg --files --hidden --smart-case --glob "!.git/*"
-elif type -q ag
-    set -qx FZF_DEFAULT_COMMAND ag --files --hidden --smart-case --glob "!.git/*"
+    set -Ux FZF_DEFAULT_COMMAND rg --files --hidden --smart-case --glob "!.git/*"
+else if type -q ag
+    set -Ux FZF_DEFAULT_COMMAND ag --files --hidden --smart-case --glob "!.git/*"
 else
-    set -qx FZF_DEFAULT_COMMAND grep --files --hidden --smart-case --glob "!.git/*"A
+    set -Ux FZF_DEFAULT_COMMAND grep --files --hidden --smart-case --glob "!.git/*"A
 end
 
 # fish git
-set -gx __fish_git_prompt_show_informative_status
-set -gx __fish_git_prompt_showcolorhints
+set -Ux __fish_git_prompt_show_informative_status
+set -Ux __fish_git_prompt_showcolorhints

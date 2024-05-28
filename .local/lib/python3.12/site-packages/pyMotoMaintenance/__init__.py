19164490d2236b8e65178d84f1bf3a26c09d4c58
LinasBeres <beres.linas@gmail.com>
2024-05-28 09:30:36 -0700

    Added local

---
+++ .local/lib/python3.12/site-packages/pyMotoMaintenance/__init__.py
@@ -0,0 +1,8 @@
+
+# The release version
+version = '0.0.1'
+__version__ = version
+
+import pyMotoMaintenance.motorcycle as motorcycle
+import pyMotoMaintenance.util as util
+import pyMotoMaintenance.create as create
